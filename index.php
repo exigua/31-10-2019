d461516945a75b8c5e5104ffe3d845c23dd26e2d
exigua <o.selcuk@it-students.fr>
2019-10-31 15:19:59 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+include 'templates/header.php'
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <ul>
+        <li class="menu"><a href="login.php">Login</a></li>
+        <li class="menu"><a href="contact.php">Contact</a></li>
+    </ul>
+    <div>
+        <h1 class="div">title
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam tempore voluptates et voluptatum facere enim recusandae incidunt, aspernatur quos sed distinctio at, facilis corporis. Eos error odit ullam recusandae voluptas.</p>
+        </h1>
+    </div>
+    <div>
+        <h2 class="div1">title
+           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem vero consectetur expedita placeat incidunt, corrupti sunt laboriosam? Ipsam sunt, sit odit dolorem sequi eveniet! Est ex nesciunt neque voluptate voluptatibus!</p> 
+        </h2>
+    </div>
+    <div>
+        <h2 class="div2">title
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione non necessitatibus perferendis molestias animi sint laudantium quam inventore earum dolore. Unde animi nihil id odio placeat qui numquam! Alias, temporibus.</p>
+        </h2>
+    </div>
+
---
+++ templates/footer.php
@@ -0,0 +1,9 @@
+
+    <footer>
+        <p>qsdqsd</p>
+    </footer>
+
+
+    <script src="js/script.js"></script>
+</body>
+</html>
